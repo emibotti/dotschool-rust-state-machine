2c81c62f602cfbded8483f54635dd76dfc83486e
Emiliano Botti <20881388+emibotti@users.noreply.github.com>
2025-07-06 16:06:19 -0300

    Adds runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,63 @@
 mod balances;
 mod system;
 
+// This is our main Runtime.
+// It accumulates all of the different pallets we want to use.
+pub struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet,
+}
+
+impl Runtime {
+	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
+	fn new() -> Self {
+		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+}
+
 fn main() {
-	println!("Hello, world!");
+	/* Create a mutable variable `runtime`, which is a new instance of `Runtime`. */
+	let mut runtime = Runtime::new();
+
+	let alice = "alice".to_string();
+	let bob = "bob".to_string();
+
+	/* Set the balance of `alice` to 100, allowing us to execute other transactions. */
+	runtime.balances.set_balance(&alice, 100);
+
+	println!("alice: {}", runtime.balances.balance(&alice));
+
+	// start emulating a block
+	/* Increment the block number in system. */
+	runtime.system.inc_block_number();
+	/* Assert the block number is what we expect. */
+	assert_eq!(runtime.system.block_number(), 1);
+
+	// first transaction
+	/* Increment the nonce of `alice`. */
+	runtime.system.inc_nonce(&alice);
+	/* Execute a transfer from `alice` to `bob` for 30 tokens.
+		- The transfer _could_ return an error. We should use `map_err` to print
+		  the error if there is one.
+		- We should capture the result of the transfer in an unused variable like `_res`.
+	*/
+	let _res = runtime
+		.balances
+		.transfer(alice.clone(), bob.clone(), 30)
+		.map_err(|err| eprintln!("{}", err));
+
+	println!("alice: {}", runtime.balances.balance(&alice));
+	println!("bob: {}", runtime.balances.balance(&bob));
+
+	// second transaction
+	/* Increment the nonce of `alice` again. */
+	runtime.system.inc_nonce(&alice);
+	/* Execute another balance transfer, this time from `alice` to `charlie` for 20. */
+	let _res = runtime
+		.balances
+		.transfer(alice.clone(), "charlie".to_string(), 20)
+		.map_err(|err| eprintln!("{}", err));
+
+	println!("alice: {}", runtime.balances.balance(&alice));
+	println!("charlie: {}", runtime.balances.balance(&"charlie".to_string()));
 }

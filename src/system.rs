2d72924eff6b205ac790299ceac4a973d63356c0
Emiliano Botti <20881388+emibotti@users.noreply.github.com>
2025-07-10 12:02:29 -0300

    Generics for system pallet

--- src/system.rs
+++ src/system.rs
@@ -1,23 +1,26 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, ops::AddAssign};
 
-type AccountID = String;
-type BlockNumber = u32;
-type Nonce = u32;
+use num::{CheckedAdd, One, Zero};
 
 /// This is the System Pallet.
 /// It handles low level state needed for your blockchain.
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountID, BlockNumber, Nonce> {
 	/// The current block number.
 	block_number: BlockNumber,
 	/// A map from an account to their nonce.
 	nonce: BTreeMap<AccountID, Nonce>,
 }
 
-impl Pallet {
+impl<AccountID, BlockNumber, Nonce> Pallet<AccountID, BlockNumber, Nonce>
+where
+	AccountID: Ord + Clone,
+	BlockNumber: Zero + Copy + AddAssign + One,
+	Nonce: Zero + CheckedAdd + One + Copy,
+{
 	/// Create a new instance of the System Pallet.
 	pub fn new() -> Self {
-		Self { block_number: 0, nonce: BTreeMap::new() }
+		Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
 	}
 
 	/// Get the current block number.
@@ -28,13 +31,16 @@ impl Pallet {
 	// This function can be used to increment the block number.
 	// Increases the block number by one.
 	pub fn inc_block_number(&mut self) {
-		self.block_number += 1;
+		self.block_number += BlockNumber::one()
 	}
 
 	// Increment the nonce of an account. This helps us keep track of how many transactions each
 	// account has made.
 	pub fn inc_nonce(&mut self, who: &AccountID) {
-		self.nonce.insert(who.clone(), *self.nonce.get(&who.clone()).unwrap_or(&0) + 1);
+		self.nonce.insert(
+			who.clone(),
+			*self.nonce.get(&who.clone()).unwrap_or(&Nonce::zero()) + Nonce::one(),
+		);
 	}
 }
 
@@ -51,7 +57,7 @@ mod test {
 			- Check the nonce of `bob` is what we expect.
 		*/
 
-		let mut system_pallet = super::Pallet::new();
+		let mut system_pallet = super::Pallet::<String, u32, u32>::new();
 		let alice = "alice".to_string();
 		let bob = "bob".to_string();
 
